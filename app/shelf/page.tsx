661345e33bed12e35922d9fc00de1907ee22da7f
Trisha Lim <hello@trishalim.com>
2024-06-26 13:13:09 +0100

    get books from shelf

---
+++ app/shelf/page.tsx
@@ -0,0 +1,42 @@
+import DeployButton from "@/components/DeployButton";
+import AuthButton from "@/components/AuthButton";
+import { createClient } from "@/utils/supabase/server";
+import FetchDataSteps from "@/components/tutorial/FetchDataSteps";
+import Header from "@/components/Header";
+import { redirect } from "next/navigation";
+
+export default async function ProtectedPage() {
+  const supabase = createClient();
+
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+
+  if (!user) {
+    return redirect("/login");
+  }
+
+
+  const { data: books } = await supabase.from('book_shelf').select().eq('user_id', user.id)
+
+  console.log(books)
+
+  return <pre>{JSON.stringify(books, null, 2)}</pre>
+
+  return (
+    <div className="flex-1 w-full flex flex-col gap-20 items-center">
+      <div className="w-full">
+        <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
+          <div className="w-full max-w-4xl flex justify-between items-center p-3 text-sm">
+            <DeployButton />
+            <AuthButton />
+          </div>
+        </nav>
+      </div>
+
+      <main className="max-w-4xl w-full">
+        <h1 className="text-3xl font-semibold">My book shelf</h1>
+      </main>
+    </div>
+  );
+}
---
+++ components/Book.tsx
@@ -0,0 +1,8 @@
+import {Book} from "@/interfaces/Book";
+
+export default function Book({ title, author }: Book ) {
+  return <div className="rounded border p-3">
+    <div className="font-semibold">{title}</div>
+    <p>{author}</p>
+  </div>
+}
