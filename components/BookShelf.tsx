79287add3748776495c7e0ab08dce4d9831ffc5e
Trisha Lim <hello@trishalim.com>
2024-07-15 17:01:39 +0100

    add button

--- components/BookShelf.tsx
+++ components/BookShelf.tsx
@@ -6,6 +6,7 @@ import { getUser } from "@/app/actions/user";
 import BooksByMonth from "@/components/BooksByMonth";
 import BooksByYear from "@/components/BooksByYear";
 import { useState } from "react";
+import Link from "next/link";
 
 export default function BookShelf() {
   const { data: user } = useQuery({
@@ -39,21 +40,31 @@ export default function BookShelf() {
 
   return (
     <div className="max-w-4xl mx-auto px-4 py-12 lg:py-16 grid gap-8">
-      <div className="flex items-baseline gap-3 border-b pb-2">
-        <button
+      <div className="flex items-baseline justify-between gap-3 border-b pb-2">
+        <div className="flex gap-3">
+          <button
+            type="button"
+            className={`${classNames.base} ${view === "yearly" && classNames.active}`}
+            onClick={() => setView("yearly")}
+          >
+            Yearly
+          </button>
+          <button
+            type="button"
+            className={`${classNames.base} ${view === "monthly" && classNames.active}`}
+            onClick={() => setView("monthly")}
+          >
+            Monthly
+          </button>
+        </div>
+
+        <Link
+          href="/add"
           type="button"
-          className={`${classNames.base} ${view === "yearly" && classNames.active}`}
-          onClick={() => setView("yearly")}
+          className="bg-black text-white px-4 py-2 font-medium rounded-md"
         >
-          Yearly view
-        </button>
-        <button
-          type="button"
-          className={`${classNames.base} ${view === "monthly" && classNames.active}`}
-          onClick={() => setView("monthly")}
-        >
-          Monthly view
-        </button>
+          Add book
+        </Link>
       </div>
 
       {data && view === "monthly" && <BooksByMonth data={data} />}
